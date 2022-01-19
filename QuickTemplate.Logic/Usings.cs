281f5a3eb5f045eb6f1355bdf75bbb387d976367
Gehrer Gerhard <g.gehrer@htl-leonding.ac.at>
2022-01-19 14:41:40 +0100

    Added global using.

--- QuickTemplate.Logic/Usings.cs
+++ QuickTemplate.Logic/Usings.cs
@@ -2,4 +2,7 @@
 //MdStart
 global using CommonBase;
 global using CommonBase.Extensions;
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 //MdEnd
